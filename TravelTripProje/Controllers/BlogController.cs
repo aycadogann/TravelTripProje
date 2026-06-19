2e1bc64f836cf90b3737643d8df96f905bd0df58
Ayça Doğan <dogan.aycaa@gmail.com>
2026-06-19 16:43:36 +0300

    Blog sayfasi olusturuldu

---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Sınıflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var degerler = context.Blogs.ToList();
+            return View(degerler);
+        }
+    }
+}
