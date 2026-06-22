f3f7fa7b5c9e56e63e72871e33b786852427655a
Ayça Doğan <dogan.aycaa@gmail.com>
2026-06-22 11:54:08 +0300

    Default controller duzenlendi. PartialView kullanildi.

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -13,13 +13,36 @@ namespace TravelTripProje.Controllers
         Context context = new Context();
         public ActionResult Index()
         {
-            var degerler = context.Blogs.ToList();
+            var degerler = context.Blogs.Take(4).ToList();
             return View(degerler);
         }
 
         public PartialViewResult Partial1()
         {
-            return PartialView();
+            var degerler = context.Blogs.OrderByDescending(x => x.ID ).Take(2).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial2()
+        {
+            var deger = context.Blogs.Where(x => x.ID == 2).ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Partial3()
+        {
+            var deger = context.Blogs.ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial4()
+        {
+            var deger = context.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial5()
+        {
+            var deger = context.Blogs.Take(3).OrderByDescending(x=>x.ID).ToList();
+            return PartialView(deger);
         }
     }
 }
